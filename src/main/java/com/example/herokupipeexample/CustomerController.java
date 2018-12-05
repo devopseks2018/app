fcdb873a441505575cf1b3eaaaf13f3ccfdf2614
devopseks2018 <devopseks@gmail.com>
2018-12-05 23:52:58 +0100

    commit

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -1,12 +1,8 @@
 package com.example.herokupipeexample;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
